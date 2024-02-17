a2ac94ac5f0d4fe88f172acdc6d85c66192ea454
strawberryhere <moones20f@gmail.com>
2024-02-17 10:36:24 +0330

    defining models

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 def index(request):
-    return HttpResponse("hello world!")
+    return HttpResponse(str(timezone.now()))
