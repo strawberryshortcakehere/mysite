39a6b0e25884f9d8ceb00bdcb42abeb7aa22cee2
strawberryhere <moones20f@gmail.com>
2024-02-20 12:47:49 +0330

    used not found request shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Question
 from .models import Quote
+from django.http import Http404
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date") [:5]
     context= {"latest_question_list": latest_question_list}
-    viewModel = {
-    "latest_question_list": latest_question_list, 
-    "quote_message": Quote.objects.order_by('?').first()
-  }
-    return render(request,"polls/index.html", viewModel)
+    quote_text={"quote_message":Quote.objects.order_by('?').first(),
+    "latest_question_list": latest_question_list}
+    # Asef wrote it like this:
+    #viewModel = {
+    #"latest_question_list": latest_question_list, 
+    #"quote_message": Quote.objects.order_by('?').first()}
+    return render(request,"polls/index.html", quote_text)
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question= get_object_or_404(Question,pk=question_id)
+    return render(request, "polls/detail.html",{"question":question})
 def results(request, question_id):
     response = "You're looking at results of question %s."
     return HttpResponse(response % question_id)
